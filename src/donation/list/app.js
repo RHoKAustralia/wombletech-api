b4c73082ec2506877438852b290d83a0b3de654b
Shaun Wilde <shaun.wilde@estimateone.com>
2021-09-11 19:15:21 +1000

    chore: introduce some basic formatting

--- src/donation/list/app.js
+++ src/donation/list/app.js
@@ -1,5 +1,7 @@
-const AWS = require('aws-sdk'); 
-const ddb = new AWS.DynamoDB.DocumentClient({region: process.env.TARGET_REGION}); 
+const AWS = require("aws-sdk");
+const ddb = new AWS.DynamoDB.DocumentClient({
+  region: process.env.TARGET_REGION,
+});
 
 // const axios = require('axios')
 // const url = 'http://checkip.amazonaws.com/';
@@ -9,44 +11,45 @@ const ddb = new AWS.DynamoDB.DocumentClient({region: process.env.TARGET_REGION})
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
  * @param {Object} event - API Gateway Lambda Proxy Input Format
  *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
  * @param {Object} context
  *
  * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
  * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
+ *
  */
 exports.lambdaHandler = async (event, context, callback) => {
-    try {
-    
-        await readMessage().then(data => {
-            // Writes each item to the console
-            data.Items.forEach(function(item) {
-                console.log(item.message)
-            });
-            callback(null, {
-                // If success return 200, and items
-                statusCode: 200,
-                body: JSON.stringify(data.Items),
-                headers: {
-                    'Access-Control-Allow-Origin': '*',
-                    'Content-Type': 'application/json'
-                },
-            })
-        }).catch((err) => {
-            // If an error occurs write to the console
-            console.error(err);
-        })
-    } catch (err) {
-        console.log(err);
-        return err;
-    }
+  try {
+    await readMessage()
+      .then((data) => {
+        // Writes each item to the console
+        data.Items.forEach(function (item) {
+          console.log(item.message);
+        });
+        callback(null, {
+          // If success return 200, and items
+          statusCode: 200,
+          body: JSON.stringify(data.Items),
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type": "application/json",
+          },
+        });
+      })
+      .catch((err) => {
+        // If an error occurs write to the console
+        console.error(err);
+      });
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
 };
 
 function readMessage() {
-    const params = {
-        TableName: 'wombletech_donation',
-        Limit: 10
-    }
-    return ddb.scan(params).promise();
+  const params = {
+    TableName: "wombletech_donation",
+    Limit: 10,
+  };
+  return ddb.scan(params).promise();
 }
